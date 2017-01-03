8ff77da5f6ef18f754d4683aa6677b3cb1fe7b96
Steven Bateman <batemanvo@gmail.com>
2017-01-02 19:23:03 -0800

    Added GET route.

--- lab-steven/route/auth-route.js
+++ lab-steven/route/auth-route.js
@@ -2,7 +2,6 @@
 
 const Router = require('express').Router;
 const debug = require('debug')('photogram:auth routes');
-const createError = require('http-errors');
 const parseJSON = require('body-parser').json();
 const basicAuth = require('../lib/basic-auth-middleware.js');
 const User = require('../model/user.js');
@@ -11,6 +10,11 @@ const authRouter = module.exports = Router();
 authRouter.post('/api/createuser', parseJSON, function(request, response, next) {
   debug('POST: /api/createuser');
 
+  if (Object.keys(request.body).length < 1) {
+    response.status(400).send('No user parameters provided.');
+    return;
+  }
+
   let user = new User(request.body);
 
   let password =  request.body.password;
@@ -18,6 +22,16 @@ authRouter.post('/api/createuser', parseJSON, function(request, response, next) 
 
   user.createHash(password)
   .then(user => user.save())
+  .then(user => user.createToken())
+  .then(token => response.send(token))
+  .catch(next);
+});
+
+authRouter.get('/api/signin', basicAuth, function(request, response, next) {
+  debug('GET: /api/signin');
+
+  User.findOne({username: request.auth.username})
+  .then(user => user.checkPassword(request.auth.password))
   .then(user => user.createToken())
   .then(token => response.send(token))
   .catch(next);
