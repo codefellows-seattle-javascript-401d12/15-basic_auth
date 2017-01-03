75648ddde06e3c0a9d0bf279c99db3389c046781
Steven Bateman <batemanvo@gmail.com>
2017-01-02 17:54:09 -0800

    Created initial server file.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const debug = require('debug')('photogram:server');
+const app = express();
+
+dotenv.load();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.listen(process.env.PORT, function() {
+  debug(`Server started on port ${process.env.PORT}`);
+});
