d76f7888e3435bc1291d3d8adab8c7fc57aaaaeb
Steven Bateman <batemanvo@gmail.com>
2017-01-02 19:33:58 -0800

    Wrote GET route tests.

--- lab-steven/test/auth-route-test.js
+++ lab-steven/test/auth-route-test.js
@@ -36,10 +36,57 @@ describe('Auth routes', function() {
     });
 
     describe('With an invalid body', function() {
-      it('should return a 401 error', done => {
+      it('should return a 400 error', done => {
         request
         .post(`${url}/api/createuser`)
         .send({username: 'Steve'})
+        .end((err, response) => {
+          expect(err).to.be.an('error');
+          expect(response.status).to.equal(400);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('GET: /api/signin', function() {
+    beforeEach(done => {
+      let user = new User(sampleUser);
+
+      user.createHash(sampleUser.password)
+      .then(user => user.save())
+      .then(user => {
+        this.tempUser = user;
+        done();
+      })
+      .catch(done);
+    });
+
+    afterEach(done => {
+      User.remove({})
+      .then(() => done())
+      .catch(done);
+    });
+
+    describe('With a valid username and password', () => {
+      it('should return a token', done => {
+        request
+        .get(`${url}/api/signin`)
+        .auth('Test user', 'Testword')
+        .end((err, response) => {
+          if (err) return done(err);
+          expect(response.status).to.equal(200);
+          expect(response.text).to.be.a('string');
+          done();
+        });
+      });
+    });
+
+    describe('With an invalid password', () => {
+      it('should return a 401 error', done => {
+        request
+        .get(`${url}/api/signin`)
+        .auth('Test  user', 'Weasel')
         .end((err, response) => {
           expect(err).to.be.an('error');
           expect(response.status).to.equal(401);
