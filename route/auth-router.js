acf1eee04f1e5df11052bfe7fac9e71aa1fc7f8e
kcirekcom <erick.f.mock@gmail.com>
2017-01-02 23:35:30 -0800

    created get/signin route method

--- route/auth-router.js
+++ route/auth-router.js
@@ -23,3 +23,13 @@ authRouter.post('/api/signup', jsonParser, function(req, res, next) {
   .then(token => res.send(token))
   .catch(next);
 });
+
+authRouter.get('/api/signin', basicAuth, function(req, res, next) {
+  debug('GET: /api/signin');
+
+  User.findOne({username: req.auth.username})
+  .then(user => user.comparePasswordHash(req.auth.password))
+  .then(user => user.generateToken())
+  .then(token => res.send(token))
+  .catch(next);
+});
