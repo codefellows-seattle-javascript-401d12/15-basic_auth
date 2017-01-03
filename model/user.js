52bd4d1ef46b79f6e3c658b08dfa0deb35cd952d
kcirekcom <erick.f.mock@gmail.com>
2017-01-02 21:48:18 -0800

    created user model with username, email, password and findHash properties/options

---
+++ model/user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const Promise = require('bluebird');
+const debug = require('debug')('cfgram:user');
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  findHash: {type: String, unique: true}
+});
