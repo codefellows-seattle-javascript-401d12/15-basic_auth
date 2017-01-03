b2c4d3963966d2ac2ff30a0a0b0e2831fb576c55
kcirekcom <erick.f.mock@gmail.com>
2017-01-02 21:06:40 -0800

    added consts and set up PORT in server file

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const debug = require('debug')('cfgram:server');
+
+dotenv.load();
+
+const PORT = process.env.PORT;
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  debug(`server running: ${PORT}`);
+});
