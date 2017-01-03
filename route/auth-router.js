d9eacdfda075b6cb04a6bd65c26ec5a1988fa075
kcirekcom <erick.f.mock@gmail.com>
2017-01-02 23:29:05 -0800

    created post/signup route method

--- route/auth-router.js
+++ route/auth-router.js
@@ -8,3 +8,18 @@ const basicAuth = require('../lib/basic-auth-middleware.js');
 const User = require('../model/user.js');
 
 const authRouter = module.exports = Router();
+
+authRouter.post('/api/signup', jsonParser, function(req, res, next) {
+  debug('POST: /api/signup');
+
+  let password = req.body.password;
+  delete req.body.password;
+
+  let user = new User(req.body);
+
+  user.generatePasswordHash(password)
+  .then(user => user.save())
+  .then(user => user.generateToken())
+  .then(token => res.send(token))
+  .catch(next);
+});
