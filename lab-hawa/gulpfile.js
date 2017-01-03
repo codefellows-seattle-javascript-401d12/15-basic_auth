68c4485229db544cecae0678d4f34861bc2b97a4
Hawa Abdi <abdih17@hotmail.com>
2017-01-03 08:28:15 -0800

    Tests passing

---
+++ lab-hawa/gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+//TODO:watch each individual file??
+
+gulp.task('test', function(){
+  gulp.src('./test/*-test.js', {read: false})
+  .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('lint', function(){
+  return gulp.src(['**/*.js','!node_modules/**'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('dev', function(){
+  gulp.watch(['**/*.js','!node_modules/**'], ['test', 'lint']);
+});
+
+gulp.task('default', ['dev']);
