a6c3e99c4927ee911b70c9d2167e55456231df11
Daniel Becker <dbecker4130@gmail.com>
2017-01-03 08:35:30 -0800

    error-middleware added to lib and required in on server, added debug to package.json

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Promise = require('bluebird');
 const debug = require('debug')('cfgram:server');
 
+const errors = require('./lib/error-middleware');
 
 const PORT = process.env.PORT;
 const app = express();
@@ -13,6 +17,11 @@ dotenv.load();
 
 mongoose.connect(process.env.MONGODB_URI);
 
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(errors);
+
 app.listen(PORT, () => {
-  console.log(`server up on ${PORT}`);
+  debug(`server up on ${PORT}`);
 });
