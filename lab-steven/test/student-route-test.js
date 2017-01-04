fa48258dae57bd8bca8765837a002372f04dae84
Steven Bateman <batemanvo@gmail.com>
2017-01-03 19:03:05 -0800

    Created User constructor beforeEach.

--- lab-steven/test/student-route-test.js
+++ lab-steven/test/student-route-test.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const User = require('../model/user.js');
+const Student = require('../model/student.js');
+const url = `http://localhost:${process.env.PORT}`;
+
+require('../server.js');
+
+const sampleUser = {
+  username: 'Test user',
+  email: 'Test@test.com',
+  password: 'Testword'
+};
+
+const sampleStudent = {
+  name: 'Test student',
+  age: 99
+};
+
+describe('Student routes', function() {
+  beforeEach(done => {
+    new User(sampleUser)
+    .createHash(sampleUser.password)
+    .then(user => {
+      this.tempUser = user;
+      return user.createToken();
+    })
+    .then(token => {
+      this.tempToken = token;
+      done();
+    })
+    .catch(done);
+  });
+});
