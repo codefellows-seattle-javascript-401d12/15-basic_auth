3deeadf65080e84277dd2aed72a177f98688ca81
Steven Bateman <batemanvo@gmail.com>
2017-01-03 18:47:53 -0800

    Added student POST and GET routes.

---
+++ lab-steven/route/student-route.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Router = require('express').Router;
+const debug = require('debug')('photogram:student route');
+const createError = require('http-errors');
+const parseJSON = require('body-parser').json();
+const bearAuth = require('../lib/bearer-auth-middleware.js');
+const Student = require('../model/student.js');
+const studentRouter = module.exports = Router();
+
+studentRouter.post('/api/student', bearAuth, parseJSON, function(request, response, next) {
+  debug('POST: /api/student');
+
+  request.body.userID = request.user._id;
+  new Student(request.body).save()
+  .then(student => response.json(student))
+  .catch(next);
+});
+
+studentRouter.get('/api/student', bearAuth, function(request, response, next) {
+  debug('GET: /api/student');
+
+  Student.find()
+  .then(arrayOfStudents => response.send(arrayOfStudents.map(student => student._id)))
+  .catch(next);
+});
