0cc27c851d7d9ea23064f0c2d859b20049f116ba
Steven Bateman <batemanvo@gmail.com>
2017-01-03 19:08:05 -0800

    Created POST test with valid body.

--- lab-steven/test/student-route-test.js
+++ lab-steven/test/student-route-test.js
@@ -2,9 +2,13 @@
 
 const expect = require('chai').expect;
 const request = require('superagent');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
 const User = require('../model/user.js');
 const Student = require('../model/student.js');
 const url = `http://localhost:${process.env.PORT}`;
+
+mongoose.Promise = Promise;
 
 require('../server.js');
 
@@ -32,5 +36,43 @@ describe('Student routes', function() {
       done();
     })
     .catch(done);
+  });
+
+  describe('POST: /api/student', () => {
+    describe('With a valid body', () =>  {
+      before(done => {
+        sampleStudent.userID = this.tempUser._id;
+        new Student(sampleStudent).save()
+        .then(student => {
+          this.tempStudent = student;
+          done();
+        })
+        .catch(done);
+      });
+
+      after(done => {
+        Promise.all([
+          Student.remove({}),
+          User.remove({})
+        ])
+        .then(() => done())
+        .catch(done);
+      });
+
+      it('should return a student', done => {
+        request
+        .post(`${url}/api/student`)
+        .set({authorization: `Bearer ${this.tempToken}`})
+        .send(sampleStudent)
+        .end((err, response) => {
+          if (err) return done(err);
+          expect(response.status).to.equal(200);
+          expect(response.body.name).to.equal(sampleStudent.name);
+          expect(response.body.age).to.equal(sampleStudent.age);
+          expect(response.body.userID).to.equal(this.tempUser._id.toString());
+          done();
+        });
+      });
+    });
   });
 });
