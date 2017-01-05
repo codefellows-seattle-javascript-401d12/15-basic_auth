3eec168d03dba7d77eb6c560c4572d060ddd86c0
kcirekcom <erick.f.mock@gmail.com>
2017-01-04 22:06:15 -0800

    set up consts in manuscript router

---
+++ route/manuscript-router.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const del = require('del');
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const Router = require('express').Router;
+const createError = require('http-errors');
+const debug = require('debug')('cfgram:manuscript-router');
+
+const Manuscript = require('../model/manuscript.js');
+const Publisher = require('../model/publisher.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const s3 = new AWS.S3();
+const dataDir = `${__dirname}/../data`;
+const upload = multer({dest: dataDir});
+
+const manuscriptRouter = module.exports = Router();
