a2d70a4658f1a75ad08c8062735794204d9ca32e
Bhavya Bharti <bhavyab@outlook.com>
2017-01-07 14:04:50 -0800

    all test passes

--- test/auth-route-test.js
+++ test/auth-route-test.js
@@ -1,105 +1,106 @@
-// 'use strict';
-//
-//
-// const expect = require('chai').expect;
-// const mongoose = require('mongoose');
-// const Promise = require('bluebird');
-// const request = require('superagent');
-// const User = require('../model/user.js');
-// const debug = require('debug')('cfgram:auth-route-test');
-//
-// mongoose.Promise = Promise;
-//
-// require('../server.js');
-//
-// const url = `http://localhost:${process.env.PORT}`;
-//
-// const exampleUser = {
-//   username: 'exampleUser',
-//   password: '1234',
-//   email: 'exampleuser@test.com'
-// };
-//
-// describe('Auth routes', function(){
-//   describe('POST: /api/signup', function(){
-//     debug('POST: /api/signup');
-//
-//     after( done => {
-//       User.remove({})
-//       .then( () => done())
-//       .catch(done);
-//     });
-//     it('should say invalid path', done => {
-//       request.post(`${url}/api/sig`)
-//       .end((err, res) => {
-//         if(err) return done(err);
-//         expect(res.status).to.equal(400);
-//       });
-//       done();
-//     });
-//
-//     it('should say body is invalid', done => {
-//       request.post(`${url}/api/signup`)
-//       .end((err, res) => {
-//         if(err) return done(err);
-//         expect(res.status).to.equal(401);
-//       });
-//       done();
-//     });
-//     it('should return a token', done => {
-//       request.post(`${url}/api/signup`)
-//       .send(exampleUser)
-//       .end((err, res) => {
-//         if(err) return done(err);
-//         expect(res.status).to.equal(200);
-//         expect(res).to.have.property('text');
-//       });
-//       done();
-//     });
-//   });
-//   describe('GET: /api/signin', function(){
-//     debug('GET: /api/signin');
-//     before( done => {
-//       new User(exampleUser).save()
-//      .then(user => {
-//        this.tempUser = user;
-//        done();
-//      })
-//      .catch(done);
-//     });
-//     after( done => {
-//       User.remove({})
-//       .then( () => done())
-//       .catch(done);
-//     });
-//     it('should say invalid path', done => {
-//       request.get(`${url}/api/sig`)
-//       .end((err, res) => {
-//         if(err) return done(err);
-//         expect(res.status).to.equal(404);
-//       });
-//       done();
-//     });
-//     it('should say user could not be authenticated', done => {
-//       request.get(`${url}/api/signin`)
-//       .send({'--auth': 'exampleUser:123'})
-//       .end((err, res) => {
-//         if(err) return done(err);
-//         expect(res.status).to.equal(401);
-//       });
-//       done();
-//     });
-//
-//
-//     it('should get a token', done => {
-//       request.get(`${url}/api/signin`)
-//       .send({'--auth': 'exampleUser:1234'})
-//       .end((err, res) => {
-//         if(err) return done(err);
-//         expect(res.status).to.equal(200);
-//         expect(res).to.have.property('text');
-//       });
-//       done();
-//     });
-//   });
-// });
+'use strict';
+
+
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const request = require('superagent');
+const User = require('../model/user.js');
+const debug = require('debug')('cfgram:auth-route-test');
+
+mongoose.Promise = Promise;
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const testUser = {
+  username: 'test-User',
+  password: '1234',
+  email: 'test-user@test.com'
+};
+
+describe('Auth routes', function(){
+  User.remove({});
+  describe('POST: /api/signup', function(){
+    after( done => {
+      User.remove({})
+      .then( () => done())
+      .catch(done);
+    });
+    debug('POST: /api/signup');
+
+    it('should say invalid path', done => {
+      request.post(`${url}/api/sig`)
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      });
+    });
+
+    it('should say body is invalid', done => {
+      request.post(`${url}/api/signup`)
+      .end((err, res) => {
+        expect(res.status).to.equal(500);
+        done();
+      });
+    });
+    it('should return a token', done => {
+      request.post(`${url}/api/signup`)
+      .send(testUser)
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res).to.have.property('text');
+        done();
+      });
+    });
+  });
+  describe('GET: /api/signin', function(){
+    debug('GET: /api/signin');
+    before( done => {
+      var user = new User(testUser);
+      user.generatePasswordHash(user.password)
+      .then( () => user.save())
+      .then( () => user.generateToken())
+      .then( token => {
+        this.token = token;
+        done();
+      })
+      .catch(done);
+    });
+    after( done => {
+      User.remove({})
+      .then( () => done())
+      .catch(done);
+    });
+    it('should say invalid path', done => {
+      request.get(`${url}/api/sig`)
+      .set({Authorization: `Basic ${this.token}`})
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.text).to.equal('Cannot GET /api/sig\n');
+        done();
+      });
+    });
+
+    it('should say UnauthorizedError', done => {
+      request.get(`${url}/api/signin`)
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.text).to.equal('UnauthorizedError');
+      });
+      done();
+    });
+    it('should return a token', done => {
+      request.get(`${url}/api/signin`)
+      .set({Authorization: `Basic ${this.token}`})
+      .send(testUser)
+      .end((err, res) => {
+        if(err) return done(err);
+        console.log('***************', res.text);
+        expect(res.status).to.equal(200);
+        expect(res).to.have.property('text');
+      });
+      done();
+    });
+  });
+});
